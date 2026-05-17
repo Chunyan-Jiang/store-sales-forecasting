c7f77bb506fec0185f994e24e82158dd409dd88a
BrokenWords <jiangcyenemma@gmail.com>
2026-05-17 21:40:23 +0200

    data load funcs

--- src/data.py
+++ src/data.py
@@ -4,27 +4,32 @@ from config import DATA_RAW
 
 
 def load_train() -> pd.DataFrame:
-    data = pd.read_csv(DATA_RAW['train'])
+    data = pd.read_csv(DATA_RAW / "train.csv")
     return data
 
 def load_test() -> pd.DataFrame:
-    pass
+    data = pd.read_csv(DATA_RAW / "test.csv")
+    return data
 
 
 def load_stores() -> pd.DataFrame:
-    pass
+    data = pd.read_csv(DATA_RAW / "stores.csv")
+    return data
 
 
 def load_oil() -> pd.DataFrame:
-    pass
+    data = pd.read_csv(DATA_RAW / "oil.csv")
+    return data
 
 
 def load_holidays() -> pd.DataFrame:
-    pass
+    data = pd.read_csv(DATA_RAW / "holidays_events.csv")
+    return data
 
 
 def load_transactions() -> pd.DataFrame:
-    pass
+    data = pd.read_csv(DATA_RAW / "transactions.csv")
+    return data
 
 
 def build_base_table(is_train: bool = True) -> pd.DataFrame:
